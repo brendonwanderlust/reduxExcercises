974fafda0b47bdc204d423635699954b744c105d
Brendon Carrasquillo <brendon.wanderlust@gmail.com>
2018-06-18 22:28:39 -0400

    first commit

---
+++ public/exercise2.js
@@ -0,0 +1,39 @@
+// EXERCISE s
+// The increment button works, but let's hook up the decrement button
+
+let reducer2 = (state = 0, action) => {
+	if (action.type == "INCREMENT") {
+		return state + 1
+	} else if (action.type == "DECREMENT") {
+		return state - 1
+	} else {
+		return state;
+	}
+	
+}
+
+let store2 = Redux.createStore(reducer2);
+
+let incrementHTML = document.getElementById('counter2-increment');
+let decrementHTML = document.getElementById('counter2-decrement');
+
+let counterHTML = document.getElementById('counter2-text');
+
+store2.subscribe(()=>{
+	let counterValue = store2.getState();
+	counterHTML.innerHTML = counterValue;
+})
+
+// we need another button listener here for the decrement button.
+// It'll dispatch a different action than this one
+incrementHTML.addEventListener('click', (e)=>{
+	store2.dispatch({
+		type: "INCREMENT"
+	})
+})
+
+decrementHTML.addEventListener("click", (e)=>{
+	store2.dispatch({
+		type: "DECREMENT"
+	})
+})
